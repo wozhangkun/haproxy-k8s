74cde28d7fd00f5486acf86cedf2e372bd77f13f
admin <34087902+wozhangkun@users.noreply.github.com>
2019-05-30 13:44:05 +0800

    Update start-haproxy.sh

--- start-haproxy.sh
+++ start-haproxy.sh
@@ -4,4 +4,4 @@ MasterIP2=192.168.9.12
 MasterIP3=192.168.9.13
 MasterPort=6443
 
-docker run -d --restart=always --name HAProxy-K8S -p 6444:6444 -e MasterIP1=$MasterIP1 -e MasterIP2=$MasterIP2 -e MasterIP3=$MasterIP3 -e MasterPort=$MasterPort wise2c/haproxy-k8s
+docker run -d --restart=always --name HAProxy-K8S -p 6444:6444 -e MasterIP1=$MasterIP1 -e MasterIP2=$MasterIP2 -e MasterIP3=$MasterIP3 -e MasterPort=$MasterPort wozhangkun/haproxy-k8s
